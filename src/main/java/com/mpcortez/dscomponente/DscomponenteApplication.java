5309d0ca4fed22b9472ecddf77e7bc7704632af3
Mpcortez <murilo_pass@hotmail.com>
2023-10-04 20:26:15 -0300

    first commit

---
+++ src/main/java/com/mpcortez/dscomponente/DscomponenteApplication.java
@@ -0,0 +1,13 @@
+package com.mpcortez.dscomponente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscomponenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscomponenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpcortez/dscomponente/DscomponenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpcortez.dscomponente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscomponenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
