7dec3e893de8476bdf99c6014892d3229e7c9593
Mpcortez <murilo_pass@hotmail.com>
2023-11-05 15:44:18 -0300

    Handling entry exception

---
+++ src/main/java/com/mpcortez/dscomponente/exception/EntryException.java
@@ -0,0 +1,8 @@
+package com.mpcortez.dscomponente.exception;
+
+public class EntryException extends RuntimeException {
+
+    public EntryException(String message) {
+        super(message);
+    }
+}
